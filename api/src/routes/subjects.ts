8dee977ea14938a8a197bb57296e5d4e4e34ed27
James Potter <pj542859@gmail.com>
2026-06-21 11:44:39 -0400

    quick fix, not caught by pnpm -r build for some reason

--- api/src/routes/subjects.ts
+++ api/src/routes/subjects.ts
@@ -12,7 +12,7 @@ const schema = z.object({
   name: z.string()
     .min(3, { message: "Subject must be at least 3 characters long."})
     .max(35, { message: "Subject can be no longer than 35 characters."})
-}
+})
 
 export const subjectsRouter = new Hono<HonoEnv>()
   // List all subjects
